638c7585e3cc4338358903b3d83db23b57d095d9
Irina-Dehtiarenko <93826301+Irina-Dehtiarenko@users.noreply.github.com>
2023-01-12 19:19:19 +0100

    examples of construction function and created objects instance of this prototype

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+// construction function(not array function)
+
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  //   Bad practise - never created methods inside the construction function  - tworzy się za każdym razem tworzenia nowego objektu
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
+};
+
+const iryna = new Person('Iryna', 1998); //it is an instance of person
+console.log(iryna);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to ptototype
+// 4. function autometicaly return{}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+
+console.log(matilda, jack);
+
+const jay = 'Jay';
+console.log(iryna instanceof Person); //true
+console.log(jay instanceof Person); //false
