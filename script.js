164b3e6275f5e20f4b6ed04d8486793d03760e08
Irina-Dehtiarenko <93826301+Irina-Dehtiarenko@users.noreply.github.com>
2023-01-15 18:51:32 +0100

    some example of the prototyple inheritance

--- script.js
+++ script.js
@@ -53,3 +53,38 @@ console.log(iryna, matilda.species);
 console.log(iryna.hasOwnProperty('firstName')); ///true
 
 console.log(iryna.hasOwnProperty('species')); //false - ponieważ nie jest bezpośrednią właściwością, tylko właściwością prototypu
+
+console.log(iryna.__proto__); //
+
+console.log(iryna.__proto__.__proto__); //Object.prototype(top of prototype chain)
+
+console.log(iryna.__proto__.__proto__.__proto__); //null
+
+console.dir(Person.prototype.constructor);
+
+const arr = [233, 546, 32, 21, 3, 1, 1, 1]; // new Array === []
+console.log(arr.__proto__); //tutaj mamy wszystkie potrzebne metody
+console.log(arr.__proto__ === Array.prototype); //true
+
+console.log(arr.__proto__.__proto__);
+// Możemy stworzyć własną metode na prototype kontsruktora, która będzie dostępna dla wszystkich objektów tego konstruktora, ale zwykle to nie jest dobry pomysł
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+}; //zwróci tylko unikalne wartości
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+
+console.dir(h1); //zwraca object ze wszystkimi właściwościami
+console.dir(h1.__proto__); //zwraca object - HTMLHeadingElement
+console.dir(h1.__proto__.__proto__); //HTMLElement
+console.dir(h1.__proto__.__proto__.__proto__); //Element
+console.dir(h1.__proto__.__proto__.__proto__.__proto__); //Node
+console.dir(h1.__proto__.__proto__.__proto__.__proto__.__proto__); //EventTarget
+console.dir(h1.__proto__.__proto__.__proto__.__proto__.__proto__.__proto__); //Object
+console.dir(
+  h1.__proto__.__proto__.__proto__.__proto__.__proto__.__proto__.__proto__
+); //null
+
+console.dir(x => x + 1); //anonymous()
+console.dir((x => x + 1).__proto__.__proto__); //Object
