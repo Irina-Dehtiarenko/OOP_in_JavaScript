1e154bf85c4b09d1f7b333d87b7c6ef6f4d187f6
Irina-Dehtiarenko <93826301+Irina-Dehtiarenko@users.noreply.github.com>
2023-01-15 18:07:46 +0100

    some examples with prototypes and some property

--- script.js
+++ script.js
@@ -29,3 +29,27 @@ console.log(matilda, jack);
 const jay = 'Jay';
 console.log(iryna instanceof Person); //true
 console.log(jay instanceof Person); //false
+
+// Prototypes
+console.log(Person.prototype); //tutaj już istnieje metoda calcAge()
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+iryna.calcAge(); //zwraca wiec
+matilda.calcAge();
+console.log(iryna.__proto__); // - to jest prototyp obiektu, to samo co właściwość prototypu funkcji konstruktora
+//{calcAge: ƒ, constructor: ƒ}
+// calcAge: ƒ ()
+// constructor: ƒ (firstName, birthYear)
+// [[Prototype]]: Object
+console.log(iryna.__proto__ === Person.prototype); //true
+console.log(Person.prototype.isPrototypeOf(iryna)); //true
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(iryna, matilda.species);
+
+console.log(iryna.hasOwnProperty('firstName')); ///true
+
+console.log(iryna.hasOwnProperty('species')); //false - ponieważ nie jest bezpośrednią właściwością, tylko właściwością prototypu
